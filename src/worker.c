0682b14165703cf2a7ba31c85b91f229cdae5f11
Daniel Kelley <dkelley@gmp.san-jose.ca.us>
2021-12-04 09:40:57 -0800

    Worker interface

---
+++ src/worker.c
@@ -0,0 +1,91 @@
+/*
+   worker.c
+
+   Copyright (c) 2021 by Daniel Kelley
+
+*/
+
+#include <assert.h>
+#include <stdlib.h>
+#include "worker.h"
+
+#define MAXW 10
+
+struct worker {
+    int count;
+    struct {
+        int fd;
+        wfunc func;
+        void *arg;
+    } w[MAXW];
+};
+
+struct worker *worker_init(void)
+{
+    struct worker *worker;
+
+    worker = calloc(1,sizeof(*worker));
+    assert(worker);
+
+    return worker;
+}
+
+void worker_done(struct worker *worker)
+{
+    if (worker) {
+        free(worker);
+    }
+}
+
+int worker_add(struct worker *worker, int fd, wfunc func, void *arg)
+{
+    int err = 1;
+
+    assert(worker);
+    if (worker->count < MAXW) {
+        worker->w[worker->count].fd = fd;
+        worker->w[worker->count].func = func;
+        worker->w[worker->count].arg = arg;
+    }
+    return err;
+}
+
+int worker_count(struct worker *worker)
+{
+    assert(worker);
+    return worker->count;
+}
+
+int worker_getfd(struct worker *worker, int idx)
+{
+    int fd;
+    assert(worker);
+    assert(idx >= 0 && idx < worker->count);
+    fd = worker->w[idx].fd;
+    assert(fd > 0);
+    return fd;
+}
+
+int worker_call(struct worker *worker, int idx)
+{
+    assert(worker);
+    assert(idx >= 0 && idx < worker->count);
+    assert(worker->w[idx].fd > 0);
+    return worker->w[idx].func(worker->w[idx].arg);
+}
+
+int worker_remove(struct worker *worker, int idx)
+{
+    assert(worker);
+    assert(idx >= 0 && idx < worker->count);
+    worker->w[idx].fd = -1;
+    if (idx == (worker->count-1)) {
+        /* last */
+        --worker->count;
+    }
+
+
+    return 0;
+}
+
+
---
+++ src/worker.h
@@ -0,0 +1,26 @@
+/*
+   worker.h
+
+   Copyright (c) 2021 by Daniel Kelley
+
+   Worker functions. This uses a file descriptor to indicate via
+   select() when some work is ready to be done.
+
+*/
+
+#ifndef   WORKER_H_
+#define   WORKER_H_
+
+struct worker;
+
+typedef int (*wfunc)(void *arg);
+
+extern struct worker *worker_init(void);
+extern void worker_done(struct worker *worker);
+extern int worker_add(struct worker *worker, int fd, wfunc func, void *arg);
+extern int worker_count(struct worker *worker);
+extern int worker_getfd(struct worker *worker, int idx);
+extern int worker_call(struct worker *worker, int idx);
+extern int worker_remove(struct worker *worker, int idx);
+
+#endif /* WORKER_H_ */
